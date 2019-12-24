ffaf8758f6300bf450bf6f5f72197b00cf7d4e61
rayna williams <Raynawilliams123@gmail.com>
2019-12-23 18:00:45 -0700

    origin.master

---
+++ script.js
@@ -0,0 +1,21 @@
+//variables
+var sun = document.getElementById("sunday");
+var mon = document.getElementById("monday");
+var tue = document.getElementById("tuesday");
+var wed = document.getElementById("wednesday");
+var thur = document.getElementById("thursday");
+var fri = document.getElementById("friday");
+var sat = document.getElementById("saturday");
+
+//add a click event for each button so that you are able to click on that section of that day to add whatever you would like to
+
+
+
+
+
+//add functions to be able to help save in the correct day 
+
+
+
+
+//use local storage to save all items for that week
